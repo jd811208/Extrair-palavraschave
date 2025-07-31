c941015e1118df55ea5619f9c9ab9e3e74c25bac
jd811208 <jonas.dutra.lopes@escola.pr.gov.br>
2025-07-31 12:29:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,5 +9,25 @@ function mostraPalavrasChave() {
 
   const palavras = texto.split(" ");
 
-  campoResultado.textContent = palavras.join(", ");
+  campoResultado.textContent = palavras;
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(/\P{L}+/u);
+
+  let frequencias = {};
+
+  for (let i of palavras) {
+    frequencias[i] = 0;
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
+    }
+  }
+
+  console.log(frequencias);
+
+  return palavras;
 }
